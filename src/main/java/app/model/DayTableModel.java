c23a4bbbc1ef73b94b49984f569b1cbd6a7bc422
Duglas <truevladimir93@yandex.ru>
2020-02-10 17:55:31 +0300

    начал создавать вебовую часть

--- src/main/java/app/model/DayTableModel.java
+++ src/main/java/app/model/DayTableModel.java
@@ -1,14 +1,26 @@
 package app.model;
 
 import app.dao.DAO;
+import app.entities.Action;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class DayTableModel {
     private static DayTableModel instance;
+    private DAO dao;
+    private List <Action> actionList;
 
     static {
         instance = new DayTableModel();
-        DAO dao;
     }
 
-    //TODO
+    public DayTableModel() {
+        this.dao = new DAO();
+        this.actionList = new LinkedList<>();
+    }
+
+    public void addAction(Action action){
+        dao.addAction(action);
+    }
 }
