2faf1cf69233f3487593be9b4aa16d0976c04a61
Duglas <truevladimir93@yandex.ru>
2020-02-10 17:21:01 +0300

    изменил файл

--- src/main/java/app/dao/DAO.java
+++ src/main/java/app/dao/DAO.java
@@ -31,19 +31,5 @@ public class DAO {
             e.printStackTrace();
         }
     }
-    /*public void addUser(User user) {
-        String name = user.getName();
-        String passwords = user.getPassword();
-        String sql = "INSERT INTO users_table (name, pass) VALUES (?, ?)";
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setString(1, name);
-            preparedStatement.setString(2, passwords);
-            preparedStatement.executeUpdate();
-            System.out.println("Пользователь добвален");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Что-то не так");
-        }
-    }*/
+
 }
